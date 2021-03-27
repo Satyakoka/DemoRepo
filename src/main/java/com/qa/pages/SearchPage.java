34bc92a0f6e9447873a684ba750094c5a4952825
Tejoma <74166389+Tejoma@users.noreply.github.com>
2021-03-27 11:17:24 -0500

    Update SearchPage.java
    changed the code to PULL the updated version

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -3,6 +3,7 @@ package com.qa.pages;
 public class SearchPage {
 	public static void Searchpage() {
 		System.out.println("This is my search page");
+		System.out.println("I am changing the code and commiting the changes to PULL the updated one to my eclipse ");
 	}
 
 }
