2c660df284eb1e10ddeb5210e4582b0fc53881ec
Tejoma <tejomayi.koka@gmail.com>
2021-03-28 19:28:12 -0500

    I have changed the code in localmachine and pushing the updated one in git repo

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -3,6 +3,7 @@ package com.qa.pages;
 public class LoginPage {
 	public static void LoginPage() {
 		System.out.println("This is login page");
+		System.out.println("This is login page testing for git changes");
 	}
 	public static void LogimPageversion() {
 		System.out.println("This is login page next version");
